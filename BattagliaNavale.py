494bca9caa19d0a254c24ad424efaa4faa198472
Nico9612 <nicolauba@gmail.com>
2023-03-25 12:59:48 +0100

    battaglia navale funzionante le navi non si sovrappongono ma si toccano

--- BattagliaNavale.py
+++ BattagliaNavale.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from random import randint
 import sys
 
@@ -11,64 +12,90 @@ class Mappa:
     #creo la mappa vuota
     def __init__(self,intervallo):
         self.intervallo=intervallo
-        self.mappa = [ [0 for i in range(self.intervallo)]  for i in range(self.intervallo)]
+        self.mappa = [ ["-" for i in range(self.intervallo)]  for i in range(self.intervallo)]
 
     #metodo per inserire una nave
     def inserisci_nave(self,nave):
-        #creo un offset di una casella per semplificare i controlli
-        i=randint(1, (self.intervallo-(nave.lenNave+1)))
-        j=randint(1, (self.intervallo-(nave.lenNave+1)))
-        verso=randint(0, 1)
-        #!!!!! Aggiungere i controlli per non far sovrapporre le navi !!!!
-        if verso == 0:
-            for k in range(nave.lenNave):
-                self.mappa[i][j+k]=1
-        else:    
-            for k in range(nave.lenNave):
-                 self.mappa[i+k][j]=1
-    
+        #!!!!! controlli per non far sovrapporre le navi !!!
+       
+        while True:
+            sovrapposte = False
+            #scelgo una posizione iniziale e un verso casuale
+            i=randint(0, (self.intervallo-(nave.lenNave)))
+            j=randint(0, (self.intervallo-(nave.lenNave)))
+            verso=randint(0, 1)
+            
+            for s in range(nave.lenNave):
+                
+                if verso==0:
+                    if j+s >= self.intervallo or self.mappa[i][j+s] != '-':
+                        sovrapposte = True
+                        break
+                    
+                   
+                        
+                else:
+                    if i+s >= self.intervallo or self.mappa[i+s][j] != '-':
+                        sovrapposte = True
+                        break
+
+           
+            #se non sono sovrapposte le inserisco nella mappa
+            if not sovrapposte:
+
+                if verso == 0:
+                    for k in range(nave.lenNave):
+                        self.mappa[i][j+k]="N"
+                    break
+                else:    
+                    for k in range(nave.lenNave):
+                        self.mappa[i+k][j]="N"
+                    break
+            
+           
+        
     #metodo per sparare un colpo
     def spara(self,x,y):
         self.x=x-1
         self.y=y-1
         
-        if self.mappa[self.y][self.x] == 0:
+        if self.mappa[self.y][self.x] == "-":
             self.mappa[self.y][self.x]="M"
             print("Mancato =( ")
         else:
-            self.mappa[self.y][self.x]="X"
-            if self.mappa[self.y+1][self.x]!= 1 and self.mappa[self.y-1][self.x]!= 1 and self.mappa[self.y][self.x+1]!= 1 and self.mappa[self.y][self.x-1]!= 1:  
-                print("Affondata!!!!")
-            else:
-                print("Colpita!!")
+            print("Colpita!!")
     
     #controllo se ci sono ancora pezzi di nave altrimenti stampo hai perso
     def game(self):
+        #contatore per verificare che ci siano ancora pezzi di nave (gli 1 sulla griglia)
         cont=0
         for i in range(self.intervallo):
             for j in range(self.intervallo):
 
-                if self.mappa[i][j]==1:
+                if self.mappa[i][j]=="N":
                     cont+=1
                 
         if cont > 0:
             return True        
         else:
-            print("Hai Perso")
+            print("Non Hai Più Navi Mi Dispiace Hai Perso!!")
             return False
 
     #metodo per stampare la mappa
     def stampa(self):
+        
 
         for riga in self.mappa:
+            
             for elemento in riga:
                 #stampo 0 per il mare
-                if elemento == 0:
-                    sys.stdout.write(str(0)+" ")
+   
+                if elemento == "-":
+                    sys.stdout.write("-"+" ")
                 #stampo 1 per le navi
                 
-                elif elemento == 1:
-                    sys.stdout.write(str(1)+" ")
+                elif elemento == "N":
+                    sys.stdout.write("N"+" ")
                 #stampo M per il colpo mancato
                 
                 elif elemento == "M":
@@ -89,15 +116,17 @@ if __name__=="__main__":
     
     #creo le navi
 
-    #portaerei=Nave(5)
+    portaerei=Nave(5)
     Cacciatorpediniere= Nave(2)
-    #Sottomarino=Nave(3)
+    Sottomarino=Nave(3)
 
     #posiziono le navi nella mappa
 
-   # mappa.inserisci_nave(portaerei)
+    mappa.inserisci_nave(portaerei)
+    mappa.inserisci_nave(portaerei)
     mappa.inserisci_nave(Cacciatorpediniere)
-    #mappa.inserisci_nave(Sottomarino)
+    mappa.inserisci_nave(Sottomarino)
+    mappa.inserisci_nave(Sottomarino)
     
     #ciclo infinito per stampare la mappa e i colpi
     game=True
@@ -105,5 +134,6 @@ if __name__=="__main__":
         
             mappa.stampa()
             mappa.spara(int(input(f"Inserire la coordinata X (da 1 a {mappa.intervallo}): ")),int(input(f"Inserire la coordinata Y (da 1 a {mappa.intervallo}): ")))
+        
             game = mappa.game()
     
